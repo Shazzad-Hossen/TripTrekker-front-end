925cc813d2e147b7abb1f73a8a9089fc21cd4fdf
Shazzad Hossen <tipsfire.com@gmail.com>
2023-10-13 03:35:47 +0600

    hotel owner profile and additional information page added

--- src/pages/Dashboard/Profile/AdditionalInfo.jsx
+++ src/pages/Dashboard/Profile/AdditionalInfo.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import AgencyProfile from './AgencyProfile';
+import HotelProfile from './HotelProfile';
 
 const AdditionalInfo = () => {
     const { user } = useSelector((state) => state.userInfo);
     return (
         <div>
 
-            {user.role==='agency'?<AgencyProfile/>:''}
+            {user.role==='agency'?<AgencyProfile/>:<HotelProfile/>}
             
         </div>
     );
